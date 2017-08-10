8a5bfac534bec2d20fd94e582996a2103192338e
王成 <274281610@qq.com>
2017-08-10 17:58:44 +0800

    11

--- src/App.js
+++ src/App.js
@@ -2,20 +2,65 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
+
+export default class App extends React.Component {
+    /*
+    *1.构造函数在组件被加载之前调用
+    *2.应该最先调用super(props);，不然使用this.props会有bug
+    *3.如果不需要initialize state 和bind methods,就不需要这方法
+    * */
+    constructor(props){
+        super(props);
+        this.state = {name : "jim",age : 99};//state初始化
+    }
+
+    /*在render方法之前调用，建议所有操作都在constructor中进行*/
+    componentWillMount() {
+
+    }
+
+    /*1.这里组件已经生成
+    * 2.获取数据，调用网络请求
+    * 3.在这里修改state会引起re-render*/
+    componentDidMount() {
+        this.refs.button.onclick = function () {
+            console.log('haha')
+        }
+    }
+
+    /*在这里处理关闭定时器，关闭网络请求*/
+    componentWillUnmount() {
+
+    }
+
+
   render() {
     return (
       <div className="App">
-        <div className="App-header">
+        <div className="App-header" >
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React</h2>
         </div>
-        <p className="App-intro">
+        <p className="App-intro" ref={'button'}>
           To get started, edit <code>src/App.js</code> and save to reload.
+            {this.state.name}{/*使用state*/}
         </p>
       </div>
     );
   }
 }
 
-export default App;
+
+/*参数的默认值*/
+const defaultProps ={
+    name:'默认用户',
+};
+
+/*定义接受参数的类型*/
+App.PropTypes = {
+    /*name 只能是数字类型,必须的*/
+    name:React.PropTypes.number.isRequired,
+};
+
+App.defaultProps = defaultProps;
+
---
+++ src/Test.js
@@ -0,0 +1,16 @@
+/**
+ * Created by wangc on 2017/8/10.
+ */
+import App from './App';
+import React from 'react';
+
+class Test extends App{
+    render(){
+        return(
+            <div>
+                2  jsx
+            </div>
+        );
+    }
+
+}
